0a8afeaf53eff48498429a8b3212a4de56cc05ac
pratik <pratikpj122@gmail.com>
2024-06-08 00:05:54 +0545

    feat: Add and update network-related Java classes
    - Add myAddress.java to get the local host address
    - Add network.java to get the address of www.google.com
    - Add reverseTest.java to get the canonical host name of www.hamropatro.com
    - Add InterfaceLister.java to list network interfaces
    - Add webLog.java to process server log files
    
    Closes #123

--- URLExample.java
+++ URLExample.java
@@ -8,19 +8,19 @@ import java.io.IOException;
 public class URLExample {
     public static void main(String[] args) {
         String urlString = "http://www.audubon.org/";
-        
+
         try {
             URI uri = new URI(urlString);
-            
+
             URL url = uri.toURL();
-            
+
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            
+
             int responseCode = connection.getResponseCode();
             System.out.println("Response Code: " + responseCode);
-            
+
             System.out.println("URL created successfully: " + url.toString());
-            
+
         } catch (URISyntaxException ex) {
             System.out.println("The URL is malformed: " + ex.getMessage());
 
@@ -28,7 +28,7 @@ public class URLExample {
 
             System.out.println("The URL is malformed: " + ex.getMessage());
         } catch (IOException ex) {
-			
+
             System.out.println("An I/O error occurred: " + ex.getMessage());
         }
     }
